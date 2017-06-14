8997dfc47dadfa52c1fa17b5e5b4274445079070
Anna Usova <usova@improveitgroup.com>
2017-06-14 21:27:20 +0700

    phonebook client side

--- webapp/js/phonebook.js
+++ webapp/js/phonebook.js
@@ -1,290 +1,217 @@
-$(document).ready(function () {
+// Class to represent a row in the seat reservations grid
+function Contact(id, firstName, lastName, phone, number) {
+    this.id = id;
+    this.firstName = firstName();
+    this.lastName = lastName();
+    this.phone = phone();
+    this.checked = ko.observable(false);
+    this.shown = ko.observable(true);
+    this.number = number;
+}
 
-    var phoneBook = {
-        rows: [],
-        backupRows: []
+Contact.prototype.toString = function () {
+    var note = "(";
+    note += this.firstName + ", ";
+    note += this.lastName + ", ";
+    note += this.phone;
+    note += ")";
+    return note;
+};
+
+// Overall viewmodel for this screen, along with initial state
+function PhoneBookModel() {
+    var self = this;
+
+    self.validation = ko.observable(false);
+
+    self.firstName = ko.observable("");
+    self.lastName = ko.observable("");
+    self.phone = ko.observable("");
+
+    self.filteredText = ko.observable("");
+    self.selectAll = ko.observable(false);
+
+    self.selectAll.subscribe(function (selectAllValue) {
+        _.each(self.rows(), function (contact) {
+            if (contact.shown()) {
+                contact.checked(selectAllValue);
+            }
+        })
+    });
+
+    self.calcTextForDeleteDialog = function (rows) {
+        var note = _.map(rows, function (contact) {
+            return contact.toString();
+        }).join(", ");
+        note = "[" + note + "]";
+        return note;
     };
 
-    var idSequence = 0;
+    self.firstNameError = ko.computed(function () {
+        if (self.firstName()) {
+            return {
+                message: "",
+                error: false
+            };
+        }
+        return {
+            message: "Поле Имя должно быть заполнено.",
+            error: true
+        };
+    });
 
-    var numberForDelete;
+    self.lastNameError = ko.computed(function () {
+        if (!self.lastName()) {
+            return {
+                message: "Поле Фамилия должно быть заполнено.",
+                error: true
+            };
+        }
+        return {
+            message: "",
+            error: false
+        };
+    });
 
-    function openDeleteDialog(title, content, onOk, onCancel) {
-        $(".delete-dialog").text(content).dialog({
-            autoOpen: false,
-            modal: true,
-            title: title,
-            buttons: {
-                "Удалить": function () {
-                    onOk.call();
-                    $(this).dialog("close");
-                },
-                "Отмена": function () {
-                    if (onCancel) {
-                        onCancel.call();
-                    }
-                    $(this).dialog("close");
-                }
-            }
+    self.phoneError = ko.computed(function () {
+        if (!self.phone()) {
+            return {
+                message: "Поле Телефон должно быть заполнено.",
+                error: true
+            };
+        }
+        var sameContact = _.find(self.rows(), {phone: self.phone()});
+
+        if (sameContact) {
+            return {
+                message: "Номер телефона не должен дублировать другие номера в телефонной книге.",
+                error: true
+            };
+        }
+        return {
+            message: "",
+            error: false
+        };
+    });
+
+    self.hasError = ko.computed(function () {
+        return self.lastNameError().error || self.firstNameError().error || self.phoneError().error;
+    });
+
+    self.rows = ko.observableArray([]);
+    self.selectedContacts = ko.observableArray([]);
+
+    self.textForDeleteDialog = ko.computed(function () {
+        var checkedContactList = _.filter(self.rows(), function (contact) {
+            return contact.checked() && contact.shown();
         });
-        $(".delete-dialog").dialog("open");
-    }
 
-    function openAlert(title, content, onOk) {
-        $(".alert").text(content).dialog({
-            autoOpen: false,
-            modal: true,
-            title: title,
-            buttons: {
-                "Ок": function () {
-                    if (onOk) {
-                        onOk.call();
-                    }
-                    $(this).dialog("close");
-                }
-            }
-        });
-        $(".alert").dialog("open");
-    }
-
-    function setRowsNumbers() {
-        $(".table-phonebook").find("tbody").find("tr").each(function (i, tr) {
-            $(tr).find("td:eq(1)").text(i + 1);
-        })
-    }
-
-    function resetValidation() {
-        $(".last-name-input").removeClass("input-error");
-        $(".first-name-input").removeClass("input-error");
-        $(".phone-input").removeClass("input-error");
-
-        $(".last-name-error-message").removeClass("error-message").text("");
-        $(".first-name-error-message").removeClass("error-message").text("");
-        $(".phone-error-message").removeClass("error-message").text("");
-    }
-
-
-    function allotEmptyFields(lastName, firstName, phone) {
-        if (!lastName) {
-            $(".last-name-error-message").addClass("error-message").text("Поле Фамилия должно быть заполнено.");
-            $(".last-name-input").addClass("input-error");
-        }
-        if (!firstName) {
-            $(".first-name-error-message").addClass("error-message").text("Поле Имя должно быть заполнено.");
-            $(".first-name-input").addClass("input-error");
-        }
-        if (!phone) {
-            $(".phone-error-message").addClass("error-message").text("Поле Телефон должно быть заполнено.");
-            $(".phone-input").addClass("input-error");
-        }
-    }
-
-    function convertContactToString(contact) {
-        var note = "(";
-        note += contact.firstName + ", ";
-        note += contact.lastName + ", ";
-        note += contact.phone;
-        note += ")";
-        return note;
-    }
-
-    function calcTextForDeleteDialog(checkedArray) {
-        if (checkedArray.length == 0) {
+        if (checkedContactList.length == 0) {
             return "Выберите контакты для удаления.";
-        } else if (checkedArray.length == 1) {
-            return "Вы уверены, что хотите удалить контакт:\r\n" + convertCheckedContactToString(phoneBook.rows, checkedArray) + "?";
+        } else if (checkedContactList.length == 1) {
+            return "Вы уверены, что хотите удалить контакт:\r\n" + checkedContactList[0].toString() + "?";
+        } else {
+            return "Вы уверены, что хотите удалить контакты:\r\n" + self.calcTextForDeleteDialog(checkedContactList) + "?";
         }
-        return "Вы уверены, что хотите удалить контакты:\r\n" + convertCheckedContactToString(phoneBook.rows, checkedArray) + "?";
-    }
-
-    function convertCheckedContactToString(rows, checkedArray) {
-        var note = "[";
-        for (var i = 0; i < rows.length; i++) {
-            if ($.inArray(rows[i].id.toString(), checkedArray) !== -1) {
-                note += convertContactToString(rows[i]);
-                note += ",";
-            }
-        }
-        note = note.substring(0, note.length - 1);
-        note += "]";
-        return note;
-    }
-
-    function validateCorrectPhoneField(phone) {
-        if (!phone.match(/[a-zа-я]/i)) {
-            return true;
-        }
-        $(".phone-error-message").addClass("error-message").text("Поле телефон не должно содержать буквы.");
-        $(".phone-input").addClass("input-error");
-        return false;
-    }
-
-    function validateNewPhoneField(phone) {
-        var newPhone = true;
-
-        for (var i = 0; i < phoneBook.backupRows.length; i++) {
-            if (phoneBook.backupRows[i].phone == phone) {
-                newPhone = false;
-                break;
-            }
-        }
-
-        if (!newPhone) {
-            $(".phone-error-message").addClass("error-message").text("Номер телефона не должен дублировать другие номера в телефонной книге.");
-            $(".phone-input").addClass("input-error");
-        }
-        return newPhone;
-    }
-
-    $(".select-all").change(function () {
-        var checked = $(this).is(":checked");
-        $(".check-contact").prop("checked", checked);
     });
 
-    $(".filter-button").click(function () {
-        var text = $(".filter-input").val().toLowerCase();
-        phoneBook.rows = JSON.parse(JSON.stringify(phoneBook.backupRows));
-        for (var i = phoneBook.rows.length - 1; i >= 0; i--) {
-            if (phoneBook.rows[i].firstName.toLowerCase().indexOf(text) == -1 &&
-                phoneBook.rows[i].lastName.toLowerCase().indexOf(text) == -1 &&
-                phoneBook.rows[i].phone.toLowerCase().indexOf(text) == -1) {
-                phoneBook.rows.splice(i, 1);
-            }
-        }
+    self.idSequence = 0;
 
-        renderTable();
-    });
-
-
-    $(".reset-filter-button").click(function () {
-        $(".filter-input").val("");
-        phoneBook.rows = JSON.parse(JSON.stringify(phoneBook.backupRows));
-        renderTable();
-    });
-
-
-    function collectChecked() {
-        var checkedArray = [];
-
-        $(".table-phonebook").find("tbody").find("tr").each(function (i, tr) {
-            var number = $(tr).find(".check-contact:checked").attr("number");
-            if (number) {
-                checkedArray.push(number);
-            }
-        });
-
-        return checkedArray;
-    }
-
-    $(".delete-checked-contact-button").click(function () {
-
-        var checkedArray = collectChecked();
-        var textForDeleteDialog = calcTextForDeleteDialog(checkedArray);
-        if (checkedArray.length == 0) {
-            openAlert("Нет выбранных контактов", textForDeleteDialog);
+    // Operations
+    self.addContact = function () {
+        if (self.hasError()) {
+            self.validation(true);
             return;
         }
-        openDeleteDialog("Удалить контакты", textForDeleteDialog,
+        var contact = new Contact(self.idSequence, self.firstName, self.lastName, self.phone, self.rows().length + 1);
+        self.rows.push(contact);
+        self.idSequence++;
+
+        self.firstName("");
+        self.lastName("");
+        self.phone("");
+        self.validation(false);
+    };
+
+    self.deleteContact = function (contact) {
+        var content = "Вы уверены, что хотите удалить контакт:\r\n" + contact.toString() + "?";
+        openDeleteDialog("Удалить контакт", content,
             function () {
-                var checkedArray = collectChecked();
-                deleteArrayFromRows(checkedArray, phoneBook.rows);
-                deleteArrayFromRows(checkedArray, phoneBook.backupRows);
-                renderTable();
+                self.rows.remove(contact);
+            }
+        )
+    };
+
+    self.deleteSelectedContact = function () {
+        var checkedContactList = _.filter(self.rows(), function (contact) {
+            return contact.checked() && contact.shown();
+        });
+        if (checkedContactList.length == 0) {
+            openAlert("Нет выбранных контактов", self.textForDeleteDialog());
+            return;
+        }
+        openDeleteDialog("Удалить контакты", self.textForDeleteDialog(),
+            function () {
+                self.rows(_.filter(self.rows(), function (contact) {
+                    return !contact.checked() || !contact.shown();
+                }));
             }
         );
-    });
+    };
 
+    self.filterContact = function () {
+        _.each(self.rows(), function (contact) {
+            var hidden = contact.firstName.toLowerCase().indexOf(self.filteredText().toLowerCase()) == -1 &&
+                contact.lastName.toLowerCase().indexOf(self.filteredText().toLowerCase()) == -1 &&
+                contact.phone.toLowerCase().indexOf(self.filteredText().toLowerCase()) == -1;
+            contact.shown(!hidden);
+        })
+    };
 
-    function deleteArrayFromRows(checkedArray, rows) {
-        for (var i = rows.length - 1; i >= 0; i--) {
-            if ($.inArray(rows[i].id.toString(), checkedArray) !== -1) {
-                rows.splice(i, 1);
-            }
-        }
-    }
-
-    function deleteItemFromRows(number, rows) {
-        for (var i = 0; i <= rows.length; i++) {
-            if (rows[i].id == number) {
-                rows.splice(i, 1);
-                break;
-            }
-        }
-    }
-
-    function checkById(rows, number, checked) {
-        for (var i = 0; i <= rows.length; i++) {
-            if (rows[i].id == number) {
-                rows[i].checked = checked;
-                break;
-            }
-        }
-    }
-
-    function renderTable() {
-        $(".table-phonebook-tr").remove();
-        phoneBook.rows.forEach(function (currentValue) {
-            var tr = $("<tr class='table-phonebook-tr'><td><label class='select-me-label'><input type='checkbox' class='check-contact'/></label></td><td class='number'></td><td></td><td></td><td class='phone'></td><td><button class='delete-button' type='button'>Удалить</button></td></tr>");
-            tr.find(".check-contact").prop('checked', currentValue.checked);
-            tr.find("td:eq(2)").text(currentValue.lastName);
-            tr.find("td:eq(3)").text(currentValue.firstName);
-            tr.find("td:eq(4)").text(currentValue.phone);
-
-
-            $(".table-phonebook").find("tbody").append(tr);
-
-            $(tr).find("button.delete-button").attr("number", currentValue.id);
-            $(tr).find(".check-contact").attr("number", currentValue.id);
-
-            $(tr).find(".check-contact").change(function () {
-                var id = $(this).attr("number");
-                var checked = $(this).is(":checked");
-                checkById(phoneBook.rows, id, checked);
-                checkById(phoneBook.backupRows, id, checked);
-            });
-
-
-            tr.find(".delete-button").click(function () {
-                numberForDelete = $(this).attr("number");
-                var content = calcTextForDeleteDialog([$(this).attr("number")]);
-                openDeleteDialog("Удалить контакт", content,
-                    function () {
-                        deleteItemFromRows(numberForDelete, phoneBook.rows);
-                        deleteItemFromRows(numberForDelete, phoneBook.backupRows);
-                        renderTable();
-                    }
-                )
-            });
+    self.resetFilter = function () {
+        _.each(self.rows(), function (contact) {
+            contact.shown(true);
         });
-        setRowsNumbers();
+        self.filteredText("");
     }
+}
 
-    $(".add-contact-button").click(function () {
-        resetValidation();
+ko.applyBindings(new PhoneBookModel());
 
-        var lastName = $(".last-name-input").val();
-        var firstName = $(".first-name-input").val();
-        var phone = $(".phone-input").val();
-
-
-        if (!lastName || !firstName || !phone) {
-            allotEmptyFields(lastName, firstName, phone);
-            return;
+function openDeleteDialog(title, content, onOk, onCancel) {
+    $(".delete-dialog").text(content).dialog({
+        autoOpen: false,
+        modal: true,
+        title: title,
+        buttons: {
+            "Удалить": function () {
+                onOk.call();
+                $(this).dialog("close");
+            },
+            "Отмена": function () {
+                if (onCancel) {
+                    onCancel.call();
+                }
+                $(this).dialog("close");
+            }
         }
-        if (!validateCorrectPhoneField(phone)) {
-            return;
+    });
+    $(".delete-dialog").dialog("open");
+}
+
+function openAlert(title, content, onOk) {
+    $(".alert").text(content).dialog({
+        autoOpen: false,
+        modal: true,
+        title: title,
+        buttons: {
+            "Ок": function () {
+                if (onOk) {
+                    onOk.call();
+                }
+                $(this).dialog("close");
+            }
         }
-        if (!validateNewPhoneField(phone)) {
-            return;
-        }
-
-        var row = {lastName: lastName, firstName: firstName, phone: phone, id: idSequence++, checked: false};
-
-        phoneBook.rows.push(row);
-        phoneBook.backupRows.push(row);
-
-        renderTable();
-        $(".form").find("input").val("");
-    })
-});
-
+    });
+    $(".alert").dialog("open");
+}
